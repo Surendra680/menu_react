e4f56e920845d141f7dad0413f20af13b7cfd1bd
Surendra680 <surendramaurya680@gmail.com>
2021-12-06 13:32:09 +0530

    Updates

---
+++ precache-manifest.f804a5dadf3f7be23e5f4d92d1cda143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a8024d64f397f587a1faba684017a9",
+    "url": "/menu_react/index.html"
+  },
+  {
+    "revision": "e61306b5723c6d16ca40",
+    "url": "/menu_react/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu_react/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu_react/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61306b5723c6d16ca40",
+    "url": "/menu_react/static/js/main.eab3a6e8.chunk.js"
+  },
+  {
+    "revision": "77be6877a9cd4efa3e40",
+    "url": "/menu_react/static/js/runtime-main.1acc40c1.js"
+  }
+]);
